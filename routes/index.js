12a68bdc78843d753ba32316f4a1ce164df1ff91
Zafk1eI <sintfrank@gmail.com>
2023-11-19 21:36:19 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,17 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница манго */
-router.get('/karamelka', function(req, res, next) {
+router.get('/mango_1', function(req, res, next) {
   res.send("<h1>Страница первого манго</h1>")
   });
 
   /* Страница манго */
-router.get('/karamelka', function(req, res, next) {
+router.get('/mango_2', function(req, res, next) {
   res.send("<h1>Страница второго манго</h1>")
   });
 
   /* Страница манго */
-router.get('/karamelka', function(req, res, next) {
+router.get('/mango_3', function(req, res, next) {
   res.send("<h1>Страница третьего манго</h1>")
   });
 
