008b05422aad4096e079936600d4c7c50d9f4450
Zafk1eI <sintfrank@gmail.com>
2023-11-19 21:35:17 +0300

    br_4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница манго */
+router.get('/karamelka', function(req, res, next) {
+  res.send("<h1>Страница первого манго</h1>")
+  });
+
+  /* Страница манго */
+router.get('/karamelka', function(req, res, next) {
+  res.send("<h1>Страница второго манго</h1>")
+  });
+
+  /* Страница манго */
+router.get('/karamelka', function(req, res, next) {
+  res.send("<h1>Страница третьего манго</h1>")
+  });
+
 module.exports = router;
