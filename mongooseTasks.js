7e871427353465f560313fb06401fb988dad7d5b
Zafk1eI <sintfrank@gmail.com>
2023-11-28 17:06:47 +0300

    fix callback error

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,18 +1,29 @@
+// var mongoose = require('mongoose')
+// mongoose.connect('mongodb://127.0.0.1:27017/test')
+// var schema = mongoose.Schema({ name: String })
+
+// schema.methods.taste = function(){
+//     console.log(this.get("name") + " вкусное")
+//     }    
+
+// var Mango = mongoose.model('Mango', schema)
+// var mangoes = new Mango({ name: 'Манго Кат Чу' })
+
+// mangoes.save().then(() => {
+//     mangoes.taste();
+//     console.log('saved');
+// })
+// .catch((err) => {
+// console.error(err);
+// });
+
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
-var schema = mongoose.Schema({ name: String })
-
-schema.methods.taste = function(){
-    console.log(this.get("name") + " вкусное")
-    }    
-
-var Mango = mongoose.model('Mango', schema)
-var mangoes = new Mango({ name: 'Манго Кат Чу' })
-
-mangoes.save().then(() => {
-    mangoes.taste();
-    console.log('saved');
+var Mango = require("./models/mango").Mango
+var mangoes = new Mango({
+title: "Манго Император",
 })
-.catch((err) => {
-console.error(err);
-});
+console.log(mangoes)
+mangoes.save().then(() => {
+    console.log(arguments);
+})
