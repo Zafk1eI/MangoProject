430c58694de952e53e3f8584a5b48d631ca1f32f
Zafk1eI <sintfrank@gmail.com>
2023-11-28 16:57:05 +0300

    schema methods

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,8 +1,16 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
-var Mango = mongoose.model('Mango', { name: String })
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.taste = function(){
+    console.log(this.get("name") + " вкусное")
+    }    
+
+var Mango = mongoose.model('Mango', schema)
 var mangoes = new Mango({ name: 'Манго Кат Чу' })
+
 mangoes.save().then(() => {
+    mangoes.taste();
     console.log('saved');
 })
 .catch((err) => {
