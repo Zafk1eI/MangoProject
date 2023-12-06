e2f5558b0e28bb1fa088737c76e2cfc0786c0c7e
Zafk1eI <sintfrank@gmail.com>
2023-12-06 16:21:14 +0300

    new router

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://localhost/mymango')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mangoesRouter = require('./routes/mangoes.js')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mangoes', mangoesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
