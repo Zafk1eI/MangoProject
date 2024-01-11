c86fe2208e530c3208a7a0477d2f2886573bc17c
Zafk1eI <sintfrank@gmail.com>
2024-01-11 23:31:13 +0300

    update menu

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,12 +1,10 @@
-const Mango = require("../models/mango").Mango;
+var db = require('../mySQLconnect.js');
 
-module.exports = async function(req, res, next) {
-  try {
+module.exports = function(req, res, next) {
     res.locals.nav = [];
-    const result = await Mango.find({}, { _id: 0, title: 1, nick: 1 });
-    res.locals.nav = result;
-    next();
-  } catch (err) {
-    throw err;
-  }
+    db.query('SELECT title, nick FROM mangos', function(err, result) {
+        if (err) throw err;
+        res.locals.nav = result;
+        next();
+    });
 };
